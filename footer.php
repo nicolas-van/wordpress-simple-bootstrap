e537649e40bba8510cdb1e1b62a1bfbe5ab4dca3
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2014-08-28 14:54:19 +0200

    Removed old retro-compatibility markup

--- footer.php
+++ footer.php
@@ -22,12 +22,7 @@
 			</footer> <!-- end footer -->
 		
 		</div> <!-- end #container -->
-				
-		<!--[if lt IE 7 ]>
-  			<script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
-  			<script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
-		<![endif]-->
-		
+
 		<?php wp_footer(); // js scripts are inserted using this function ?>
 
 	</body>
--- header.php
+++ header.php
@@ -1,11 +1,5 @@
 <!doctype html>  
 
-<!--[if IEMobile 7 ]> <html <?php language_attributes(); ?>class="no-js iem7"> <![endif]-->
-<!--[if lt IE 7 ]> <html <?php language_attributes(); ?> class="no-js ie6"> <![endif]-->
-<!--[if IE 7 ]>    <html <?php language_attributes(); ?> class="no-js ie7"> <![endif]-->
-<!--[if IE 8 ]>    <html <?php language_attributes(); ?> class="no-js ie8"> <![endif]-->
-<!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
-	
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
@@ -13,9 +7,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
   		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
-		<!-- wordpress head functions -->
 		<?php wp_head(); ?>
-		<!-- end of wordpress head -->
 	</head>
 	
 	<body <?php body_class(); ?>>
