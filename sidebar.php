db655f07e92236a3f1b60389602c4ac4bd162f66
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2015-01-21 21:41:53 +0100

    Corrected links in sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,4 @@
-				<div id="sidebar1" class="col-sm-4" role="complementary">
+				<div id="sidebar1" class="vertical-nav col-sm-4" role="complementary">
 				
 					<?php if ( is_active_sidebar( 'sidebar1' ) ) : ?>
 
