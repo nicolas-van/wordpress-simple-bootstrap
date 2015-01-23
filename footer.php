e344d0232ccce95643c4e0e81ad2354f3c9c4412
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2015-01-23 10:29:38 +0100

    Removed useless footers

--- footer.php
+++ footer.php
@@ -1,12 +1,9 @@
-			<footer id="inner-footer" class="row vertical-nav" role="contentinfo">
-        <hr />
-		
-        <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer1') ) : ?>
-        <?php endif; ?>
-        <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer2') ) : ?>
-        <?php endif; ?>
-        <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer3') ) : ?>
-        <?php endif; ?>
+			
+            <footer id="inner-footer" class="row vertical-nav" role="contentinfo">
+                <hr />
+        		
+                <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer1') ) : ?>
+                <?php endif; ?>
 				
 			</footer> <!-- end footer -->
 		
--- functions.php
+++ functions.php
@@ -52,8 +52,8 @@ you like. Enjoy!
 function wp_bootstrap_register_sidebars() {
     register_sidebar(array(
     	'id' => 'sidebar1',
-    	'name' => 'Main Sidebar',
-    	'description' => 'Used on every page BUT the homepage page template.',
+    	'name' => 'Sidebar',
+    	'description' => 'Used on every page.',
     	'before_widget' => '<div id="%1$s" class="widget %2$s">',
     	'after_widget' => '</div>',
     	'before_title' => '<h4 class="widgettitle">',
@@ -62,25 +62,7 @@ function wp_bootstrap_register_sidebars() {
     
     register_sidebar(array(
       'id' => 'footer1',
-      'name' => 'Footer 1',
-      'before_widget' => '<div id="%1$s" class="widget col-sm-4 %2$s">',
-      'after_widget' => '</div>',
-      'before_title' => '<h4 class="widgettitle">',
-      'after_title' => '</h4>',
-    ));
-
-    register_sidebar(array(
-      'id' => 'footer2',
-      'name' => 'Footer 2',
-      'before_widget' => '<div id="%1$s" class="widget col-sm-4 %2$s">',
-      'after_widget' => '</div>',
-      'before_title' => '<h4 class="widgettitle">',
-      'after_title' => '</h4>',
-    ));
-
-    register_sidebar(array(
-      'id' => 'footer3',
-      'name' => 'Footer 3',
+      'name' => 'Footer',
       'before_widget' => '<div id="%1$s" class="widget col-sm-4 %2$s">',
       'after_widget' => '</div>',
       'before_title' => '<h4 class="widgettitle">',
