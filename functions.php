b0a5bd304a92526156d16320a150be358adbb04c
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2015-01-23 13:31:54 +0100

    Corrected tag clouds

--- functions.php
+++ functions.php
@@ -112,41 +112,6 @@ function custom_password_form() {
 	return $o;
 }
 
-/*********** update standard wp tag cloud widget so it looks better ************/
-
-add_filter( 'widget_tag_cloud_args', 'my_widget_tag_cloud_args' );
-
-function my_widget_tag_cloud_args( $args ) {
-	$args['number'] = 20; // show less tags
-	$args['largest'] = 9.75; // make largest and smallest the same - i don't like the varying font-size look
-	$args['smallest'] = 9.75;
-	$args['unit'] = 'px';
-	return $args;
-}
-
-// filter tag clould output so that it can be styled by CSS
-function add_tag_class( $taglinks ) {
-    $tags = explode('</a>', $taglinks);
-    $regex = "#(.*tag-link[-])(.*)(' title.*)#e";
-
-    foreach( $tags as $tag ) {
-    	$tagn[] = preg_replace($regex, "('$1$2 label tag-'.get_tag($2)->slug.'$3')", $tag );
-    }
-
-    $taglinks = implode('</a>', $tagn);
-
-    return $taglinks;
-}
-
-add_action( 'wp_tag_cloud', 'add_tag_class' );
-
-add_filter( 'wp_tag_cloud','wp_tag_cloud_filter', 10, 2) ;
-
-function wp_tag_cloud_filter( $return, $args )
-{
-  return '<div id="tag-cloud">' . $return . '</div>';
-}
-
 // Enable shortcodes in widgets
 add_filter( 'widget_text', 'do_shortcode' );
 
@@ -274,19 +239,6 @@ function save_homepage_meta( $post_id ) {
     } // end foreach
 }
 add_action( 'save_post', 'save_homepage_meta' );
-
-// Add lead class to first paragraph
-// deactivated because it was ugly
-/*function first_paragraph( $content ){
-    global $post;
-
-    // if we're on the homepage, don't add the lead class to the first paragraph of text
-    if( is_page_template( 'page-homepage.php' ) )
-        return $content;
-    else
-        return preg_replace('/<p([^>]+)?>/', '<p$1 class="lead">', $content, 1);
-}
-add_filter( 'the_content', 'first_paragraph' );*/
 
 // Menu output mods
 class Bootstrap_walker extends Walker_Nav_Menu{
