2fe6abf9246a9a86d150206c08718d7ff23f25e5
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2017-01-24 13:26:24 +0100

    Minor modification in footer

--- footer.php
+++ footer.php
@@ -9,7 +9,7 @@
                         <?php dynamic_sidebar('footer1'); ?>
 
                         <div class="col-xs-12 text-center">
-                            <p><?php _e('Design by <a href="https://github.com/nicolas-van">@nicolas-van</a>', 'simple-bootstrap') ?></p>
+                            <p><?php _e('Simple Bootstrap theme by <a href="https://github.com/nicolas-van">@nicolas-van</a>', 'simple-bootstrap') ?></p>
                             <p><?php _e("Powered by WordPress", "simple-bootstrap"); ?></p>
                         </div>
                     </div>
