b0ff00ff8edf2d43a7b29b5973ff4638a1015c31
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2019-08-10 23:41:06 +0200

    corrected input group

--- searchform.php
+++ searchform.php
@@ -2,9 +2,9 @@
     <fieldset>
 		<div class="input-group">
 			<input type="text" name="s" id="search" placeholder="<?php _e("Search", "simple-bootstrap"); ?>" value="<?php the_search_query(); ?>" class="form-control" />
-			<span class="input-group-btn">
+			<div class="input-group-append">
 				<button type="submit" class="btn btn-primary"><?php _e("Search", "simple-bootstrap"); ?></button>
-			</span>
+			</div>
 		</div>
     </fieldset>
 </form>
